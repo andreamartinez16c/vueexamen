0a2f2b8b8f84236db9685967630055e74a1b5dfd
andreamartinez16c <andrea.martinez16.c@gmail.com>
2023-11-20 00:16:09 +0100

    creationCommmit

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Departamentos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/create">Create</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Departamentos
+            </a>
+            <ul class="dropdown-menu">
+              <li v-for="departamento in departamentos" :key="departamento">
+                <router-link
+                  class="dropdown-item"
+                  :to="'/detalles/' + departamento.idDepartamento"
+                  >{{ departamento.nombre }}</router-link
+                >
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/seguridad">Seguridad</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: "MenuDepartamento",
+};
+</script>
+
+<style>
+</style>
